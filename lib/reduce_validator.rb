cc23ae2ced80ee5c5fa8b5d9b57978ea96d601f3
Martin Moen Wulffeld <martin@wulffeld.dk>
2022-09-16 19:12:42 +0200

    Fix reducing
    ActiveModel::Errors has changed and #slice! would do an endless loop.

--- lib/reduce_validator.rb
+++ lib/reduce_validator.rb
@@ -2,8 +2,11 @@ require 'active_model/validator'
 
 class ReduceValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, _value)
-    return until record.errors.include?(attribute)
+    return if record.errors[attribute].size <= 1
 
-    record.errors[attribute].slice!(-1) until record.errors[attribute].size <= 1
+    first_message = record.errors[attribute][0]
+
+    record.errors.delete(attribute)
+    record.errors.add(attribute, message: first_message)
   end
 end
